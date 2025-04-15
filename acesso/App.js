9c234fe07ce63c53610e7e853c10c864029a7f4d
vinici <vinicius_apple22@outlook.com>
2025-04-14 21:22:03 -0300

    b

--- acesso/App.js
+++ acesso/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React  from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './components/Login';
+import HomeScreen from './components/Home';
+import RegistroScreen from './components/Registro';
+import PerfilScreen from './components/Perfil';
+import SplashScreen from './components/SplashScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Splash"     component={SplashScreen}    options={{headerShown: false}} />
+        <Stack.Screen name = "Login"      component={LoginScreen}     options={{headerShown: false}} />
+      <Stack.Screen name = "Registro"     component={RegistroScreen}/>
+        <Stack.Screen name = "Home"       component={HomeScreen}      options={{headerLeft: () => null}} />
+        <Stack.Screen name = "Perfil"     component={PerfilScreen} />
+      </Stack.Navigator>
+   </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
